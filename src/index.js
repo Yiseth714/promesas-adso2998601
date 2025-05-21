a2642985a236677c6768a4438f4984f692af60ee
Yiseth714 <yisethvaleguti@gmail.com>
2025-05-21 16:04:13 -0500

    segunda version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-s
+const verificarContraseña=(con)=>{
+    return new Promise((resolve,reject) =>{
+        setTimeout(() => {
+            if(con.length>=2){
+                resolve("la contraseña es valida")}
+            else{
+                reject("la contraseña no cumple")
+            }
+        }, 1000);
+    })
+}
+
+async function eliminarE(){
+    try{
+        console.log("Contraseña verificandose...")
+        const respuesta = await verificarContraseña("01234567");
+        console.log(respuesta)
+    } catch(error){
+        console.log("El archivo no pudo ser eliminado", error)
+    }
+}
+
+eliminarE()
