6e8021bdea1fe2d3b21d0864132315efb75b7846
Yiseth714 <yisethvaleguti@gmail.com>
2025-05-21 14:27:14 -0500

    Implementacion de la funcion 'Eliminar archivo'

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
-//Funcion para simular la descarga de un archivo, utilizando promesas
-function descargarArchivo(){
-    return new Promise((resolve)=>{
+function eliminarArchivo(){
+    return new Promise((resolve) =>{
         setTimeout(() => {
-            resolve("Archivo descargado con exito")
-        }, 3000);
+            resolve("Archivo eliminado")
+        }, 8000);
     })
 }
 
-// console.log(descargarArchivo().then((resultado)=>{console.log(resultado)}))
-
-
-//Funcion con Async 
-async function ejecutar(){
-    //esta promesa tiene dos tiempos, la funcion "ejecutar" debe esperar todo lo que esta adentro de ella se ejecute
-    //a su vez, el try catch tiene que esperar a que la otra funcion se ejecute
-    try {
-        console.log("Descarga en proceso")
-        const respuesta = await descargarArchivo()
+async function eliminarE(){
+    try{
+        console.log("Archivo eliminandose...")
+        const respuesta = await eliminarArchivo()
         console.log(respuesta)
-    } catch (error) {
-        console.error("La descarga ha fallado con exito", error)
+    } catch(error){
+        console.log("El archivo no pudo ser eliminado", error)
     }
 }
 
-ejecutar()
+eliminarE()
