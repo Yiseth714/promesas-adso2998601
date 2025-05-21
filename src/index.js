14cb389d4a83de9eee07465f7d5299399f0759d6
Yiseth714 <yisethvaleguti@gmail.com>
2025-05-21 14:08:07 -0500

    Inicio de Promesa con async await

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-//Funcion para simular la descarga de un archivo, utilizando promesas
+//Funcion con Async 
 
-function descargarArchivo(){
-    return new Promise((resolve)=>{
-        setTimeout(() => {
-            resolve("Archivo descargado con exito")
-        }, 3000);
-    })
+async function ejecutar(){
+    //esta promesa tiene dos tiempos, la funcion "ejecutar" debe esperar todo lo que esta adentro de ella se ejecute
+    //a su vez, el try catch tiene que esperar a que la otra funcion se ejecute
+    try {
+        console.log("Descarga en proceso")
+        const respuesta = await 
+    } catch (error) {
+        console.log("La descarga ha fallado con exito")
+    }
 }
-
-console.log(descargarArchivo().then((resultado)=>{console.log(resultado)}))
-
