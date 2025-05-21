56eaf284adca877734a5bf86f620c2f4e819473b
Yiseth714 <yisethvaleguti@gmail.com>
2025-05-21 14:14:45 -0500

    Configuracion de promesas completo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-//Funcion con Async 
+//Funcion para simular la descarga de un archivo, utilizando promesas
+function descargarArchivo(){
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve("Archivo descargado con exito")
+        }, 3000);
+    })
+}
 
+// console.log(descargarArchivo().then((resultado)=>{console.log(resultado)}))
+
+
+//Funcion con Async 
 async function ejecutar(){
     //esta promesa tiene dos tiempos, la funcion "ejecutar" debe esperar todo lo que esta adentro de ella se ejecute
     //a su vez, el try catch tiene que esperar a que la otra funcion se ejecute
     try {
         console.log("Descarga en proceso")
-        const respuesta = await 
+        const respuesta = await descargarArchivo()
+        console.log(respuesta)
     } catch (error) {
-        console.log("La descarga ha fallado con exito")
+        console.error("La descarga ha fallado con exito", error)
     }
 }
+
+ejecutar()
