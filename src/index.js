27d0fdfb913aecdbe5071624d0d3ca87c9fdfb1d
Yiseth714 <yisethvaleguti@gmail.com>
2025-05-21 14:53:13 -0500

    Retorno de si un valor es positivo o negativo

--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
-function eliminarArchivo(){
-    return new Promise((resolve) =>{
+// const boolean = (valor)=> {
+//     return new Promise((resolve, reject) =>{
+        
+//         setTimeout(() => {
+//             if(valor==true){
+//             resolve("el valor es positivo")
+//             }else{
+//                 reject("el valor es falso")
+//             }
+//         }, 2000);
+//     })
+// }
+
+// async function ejecutarBoleean(){
+//     try{
+//         console.log("Verificando...")
+//         const a= false;
+//         const respuesta = await boolean(a)
+//         console.log(respuesta)
+//     } catch(error){
+//         console.log("El valor no pudo verificarse", error)
+//     }
+// }
+
+// ejecutarBoleean()
+
+function boolean () {
+    return new Promise((resolve, reject) =>{
+        
         setTimeout(() => {
-            resolve("Archivo eliminado")
-        }, 8000);
+            const valor= true;
+            if(valor==true){
+            resolve("el valor es positivo")
+            }else{
+            reject("el valor es falso")
+            }
+        }, 2000);
     })
 }
 
-async function eliminarE(){
-    try{
-        console.log("Archivo eliminandose...")
-        const respuesta = await eliminarArchivo()
-        console.log(respuesta)
-    } catch(error){
-        console.log("El archivo no pudo ser eliminado", error)
-    }
-}
 
-eliminarE()
+boolean()
+    .then(result=>console.log(result))
+    .catch(error=>console.log(error))
